cb459c076bc80b93f446cfe63e2daba971e33001
phyozawaung005 <142676806+Ethansarfic12@users.noreply.github.com>
2025-11-10 13:02:43 +0630

    lab 7 code cov update 4

--- src/test/java/com/napier/devops/AppIntegrationTest.java
+++ src/test/java/com/napier/devops/AppIntegrationTest.java
@@ -21,24 +21,11 @@ public class AppIntegrationTest
     }
 
     @Test
-    void testGetEmployee() {
-        Employee emp = app.getEmployee(255530);
-        assertEquals(255530, emp.emp_no);
-        assertEquals("Ronghao", emp.first_name);
-        assertEquals("Garigliano", emp.last_name);
-    }
-
-    @Test
-    void testAddEmployee()
+    void testGetEmployee()
     {
-        Employee emp = new Employee();
-        emp.emp_no = 500000;
-        emp.first_name = "Kevin";
-        emp.last_name = "Chalmers";
-        app.addEmployee(emp);
-        emp = app.getEmployee(500000);
-        assertEquals(emp.emp_no, 500000);
-        assertEquals(emp.first_name, "Kevin");
-        assertEquals(emp.last_name, "Chalmers");
+        Employee emp = app.getEmployee(255530);
+        assertEquals(emp.emp_no, 255530);
+        assertEquals(emp.first_name, "Ronghao");
+        assertEquals(emp.last_name, "Garigliano");
     }
 }
